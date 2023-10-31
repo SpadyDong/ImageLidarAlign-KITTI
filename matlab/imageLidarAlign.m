5eafad2d0e6a0bb125852baf6f532395ff9215a8
SpadyDong <47657625+SpadyDong@users.noreply.github.com>
2023-10-31 10:37:48 +0800

    Update imageLidarAlign.m

--- matlab/imageLidarAlign.m
+++ matlab/imageLidarAlign.m
@@ -14,7 +14,7 @@ function image = imageLidarAlign(calibPath, imagePath, cameraIndex, lidarPath)
     P_velo_to_img = calib.("P"+cameraIndex)*calib.TrRT;
     % 对齐
     pc(:,4) = 1;
-    pc = (P_velo_to_img*pc')';
+    pc = (P_velo_to_img*pc');
     pc = pc(:,1:2)./(pc(:,3)*ones(1,2));
     pc = floor(pc);
     sel = (pc(:,1) <= 0) | (pc(:,1) > size(image, 2)) | (pc(:,2) <= 0) | (pc(:,2) > size(image, 1));
